4c95e9c4bc00b04b111fe03027ecca2660d43a17
angelSantori <antonyo.da.mm@gmail.com>
2023-11-05 18:27:00 -0700

    Componente juegodados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { JuegodadosComponent} from './juegodados/juegodados.component' //<--- Agregado
+import { AcercadeComponent } from './acercade/acercade.component' //<--- Agregado
+
+const routes: Routes = [
+  {
+    path: 'juegodados', //<--- Agregado
+    component: JuegodadosComponent //<--- Agregado
+  },
+  {
+    path:'acercade', //<--- Agregado
+    component: AcercadeComponent //<--- Agregado
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
